3897a9c426e55020049e7e3b4fbb8dbdcf8500ca
maxim3andr3 <maxim3.vd@gmail.com>
2024-07-19 18:58:38 +0200

    Write a bot that displays a greeting, its name, and the year it was created

--- src/Main.kt
+++ src/Main.kt
@@ -1,3 +1,9 @@
 fun main() {
+    val botName = "Codus"
+    val birthYear = 2042
 
+    println("""
+        Hello! My name is $botName.
+        I was created in $birthYear.
+    """.trimIndent())
 }
